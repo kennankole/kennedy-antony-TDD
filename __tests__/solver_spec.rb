0b742fe934eadd76084b35d276f626945041176d
kennankole <kennankole@gmail.com>
2023-02-09 14:21:42 +0300

    Implemented and tested the factorial logic

--- __tests__/solver_spec.rb
+++ __tests__/solver_spec.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
 require 'rspec'
-require ('./src/solver')
+require('./src/solver')
 
 describe Solver do
-  context "When testing the solver class" do
-    it "The factorial method should return 'Hello mate' " do
+  context 'When testing the solver class' do
+    it "Should return the factorial of a number " do
       fact = Solver.new
-      text = fact.factorial
-      expect(text).to eq "Hello mate"
+      expect(fact.factorial(5)).to eq(120)
     end
 
     it "The reverse method should return 'I am reverse' " do
       revert = Solver.new
       message = revert.reverse
-      expect(message).to eq "I am reverse"
+      expect(message).to eq 'I am reverse'
     end
 
     it "The fizzbuz method should return 'I am fizzbuzz' " do
       fizz = Solver.new
       message = fizz.fizzbuzz
-      expect(message).to eq "I am fizzbuzz"
+      expect(message).to eq 'I am fizzbuzz'
     end
   end
 end
--- src/solver.rb
+++ src/solver.rb
@@ -1,16 +1,21 @@
-class Solver
-  def initialize
-  end
+# frozen_string_literal: true
 
-  def factorial
-    "Hello mate"
+# A class that has factorial, reverse, and fizzbuzz methods
+class Solver
+  def initialize; end
+
+  def factorial(a)
+    return 1 if a <= 1
+    a * factorial(a - 1)
   end
 
   def reverse
-    "I am reverse"
+    'I am reverse'
   end
 
   def fizzbuzz
-    "I am fizzbuzz"
+    'I am fizzbuzz'
   end
 end
+
+puts Solver.new.factorial(5)
